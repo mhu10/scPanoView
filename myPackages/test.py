78b49a73d9e662a579cc3e700954aab0752919b5
mhu10 <38729968+mhu10@users.noreply.github.com>
2018-04-25 15:39:33 -0400

    Delete test.py

--- myPackages/test.py
+++
@@ -1,8 +0,0 @@
-from myPackages import somePython
-
-def test_fahrToKelv():
-    '''
-    make sure freezing is calculated correctly
-    '''
-    
-    assert somePython.fahrToKelv(32) == 273.15, 'incorrect freezing point!'
