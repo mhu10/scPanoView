9755cf0f9def88fe70d3d4069d07864ca4cd509e
mhu10 <38729968+mhu10@users.noreply.github.com>
2018-04-25 13:55:58 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python-package-example',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='An example python package',
+    long_description=open('README.md').read(),
+    install_requires=['numpy'],
+    url='https://github.com/mhu10/demo101mhu',
+    author='mhu10',
+    author_email='myemail@example.com'
+)
