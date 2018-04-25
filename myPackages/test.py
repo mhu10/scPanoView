391ea4148f75e7c405dfc5256fab42f9a1a73359
mhu10 <38729968+mhu10@users.noreply.github.com>
2018-04-25 13:35:08 -0400

    Create test.py

---
+++ myPackages/test.py
@@ -0,0 +1,8 @@
+from myPackage import somePython
+
+def test_fahrToKelv():
+    '''
+    make sure freezing is calculated correctly
+    '''
+    
+    assert somePython.fahrToKelv(32) == 273.15, 'incorrect freezing point!'
