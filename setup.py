04784a6c792934a4a936467ddee74f167f111103
mhu10 <38729968+mhu10@users.noreply.github.com>
2018-04-25 15:42:32 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='demo101mhu',
-    version='0.1',
+    version='0.2',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='An example python package',
