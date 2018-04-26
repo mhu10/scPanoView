ac99b721f80824a381d7458529b60f3342d342b5
mhu10 <38729968+mhu10@users.noreply.github.com>
2018-04-26 14:27:10 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='demo101mhu',
-    version='0.2',
+    name='scPanoView',
+    version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='An example python package',
     long_description=open('README.md').read(),
     install_requires=['numpy','pandas','scipy','matplotlib','seaborn','scikit-learn','statsmodels'],
-    url='https://github.com/mhu10/',
+    url='https://github.com/mhu10/scPanoView',
     author='mhu10',
     author_email='mhu10@jhmi.edu'
 )
