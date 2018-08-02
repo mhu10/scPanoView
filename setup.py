d38cd0090f88dce547bbb6a60f313646f406bd8b
mhu10 <38729968+mhu10@users.noreply.github.com>
2018-08-02 13:55:59 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description='A single-cell clustering algorithm',
     long_description=open('README.md').read(),
     install_requires=['numpy','pandas','scipy','matplotlib','seaborn','scikit-learn','statsmodels'],
-    url='https://github.com/mhu10',
+    url='https://github.com/mhu10/scPanoView',
     author='Ming-Wen Hu & Jiang Qian',
     author_email='mhu10@jhmi.edu'
 )
