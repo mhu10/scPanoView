8e55764d78ff93db3531410e1e10c8ae1e0edf58
mhu10 <38729968+mhu10@users.noreply.github.com>
2018-08-03 15:31:24 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='A single-cell clustering algorithm',
-    long_description=open('manual.pdf').read(),
+    long_description=open('README.md').read(),
     install_requires=['numpy>=1.13','pandas>=0.20','scipy>=0.19','matplotlib','seaborn>=0.8','scikit-learn>=0.19','statsmodels>=0.8'],
     url='https://github.com/mhu10/scPanoView',
     author='Ming-Wen Hu & Jiang Qian',
